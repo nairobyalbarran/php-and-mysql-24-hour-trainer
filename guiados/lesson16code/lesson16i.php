eed7ebd3a22a7aeb20628fb5906921655ef1b70f
NAIROBYALBARRAN <NAIROBYALBARRAN@Nayoba>
2014-05-06 18:21:21 +0200

    Primer commit

---
+++ guiados/lesson16code/lesson16i.php
@@ -0,0 +1,12 @@
+<?php
+$b=3;
+$c=0;
+
+if(isset($a)){
+    echo '$a equals ' . $a . '<br/>';
+}
+if (isset($b)){
+    echo '$b equals ' .$b . '<br/>';
+}if (isset($c)) {
+    echo '$c equals ' . $c . '<br/>';
+}
